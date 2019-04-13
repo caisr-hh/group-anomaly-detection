4fe93028f8842809eaf51514d8fab7b85ae9c53a
Mohamed-Rafik-Bouguelia <mohbou@hh.se>
2019-04-13 06:57:44 -0700

    Corrected package name scikit-learn in setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='cosmo',
     author_email='mohbou@hh.se',
     license='MIT',
     packages=['cosmo'],
-    install_requires=['matplotlib>=2.1.0', 'numpy>=1.13.3', 'pandas>=0.22.0', 'scipy>=1.0.0', 'sklearn>=0.20.1'],
+    install_requires=['matplotlib>=2.1.0', 'numpy>=1.13.3', 'pandas>=0.22.0', 'scipy>=1.0.0', 'scikit-learn>=0.20.0'],
     zip_safe=False)
     
